b0dbc448d9caf706965fb8ae2d2ffc743ea41ace
Praveen Raghuvanshi <praveen_iiitmk@yahoo.com>
2021-05-16 01:49:31 +0530

    Orleans: Added Hello World Sample

---
+++ drafts/Orleans/src/OrleansDemos/HelloWorldHost/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
+using Orleans.Hosting;
+
+namespace HelloWorldHost
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return RunMainAsync().Result;
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            var host = await StartSiloAsync();
+            Console.WriteLine("\n\n Silo Started Successfully, Press Enter to terminate...\n\n");
+            Console.ReadLine();
+
+            await host.StopAsync();
+
+            return 0;
+        }
+
+        private static async Task<ISiloHost> StartSiloAsync()
+        {
+            // Define cluster configuration
+            var builder = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback);
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
+        }
+    }
+}
