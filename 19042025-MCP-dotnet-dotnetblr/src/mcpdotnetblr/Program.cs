04391424cad7247813a794577c6c2272cb13c4ce
Praveen Raghuvanshi <praveenraghuvanshi@gmail.com>
2025-04-18 20:43:29 +0530

    TimeTool MCP Server
    TimeTool MCP Server

---
+++ 19042025-MCP-dotnet-dotnetblr/src/mcpdotnetblr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+
+Console.WriteLine("Starting MCP .Net BLR server...");
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+Console.WriteLine("MCP .Net BLR server stopped.");
+
+
+[McpServerToolType]
+public class TimeTool
+{
+    [McpServerTool, Description("Get the current time")]
+    public string GetCurrentTime()
+    {
+        return DateTime.UtcNow.ToString("o");
+    }
+}
